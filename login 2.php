c05926fc1b58a2cbb466d09d36e6c6b45db524b0
JATIN MALLARAPU <109145951+jatinmallarapu@users.noreply.github.com>
2024-01-19 15:31:05 +0530

    Create login 2.php

---
+++ login 2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>
+login
+</title>
+</head>
+<body>
+<h1>Login page</h1>
+<form action="welcome.php" method="POST">
+Username:<input type="text" name="uname"><br/>
+Password:<input type="password" name="pass"><br/>
+<input type="submit" nmae="login" value="login">
+</form>
+</body>
+</html>
